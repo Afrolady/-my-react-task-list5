f6c375e9770385c246adbba45aaac1ca3d6aa141
Leydi Vidal <afroladymersi2012@gmail.com>
2023-06-21 19:59:45 -0500

    buttons modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
-import Task from "./components/Task";
 import TaskList from "./components/TaskList";
-
 
 function App() {
   return (
     <>
-      
-
       <Header />
-
       <TaskList />
     </>
   );
